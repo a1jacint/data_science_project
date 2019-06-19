fd69fe26456cd114c2b5eb2f819fb6c79c5b2bb4
Adrian <a1jacint@ucsd.edu>
2019-06-18 21:34:15 -0700

    Add files via upload

---
+++ webscraper.py
@@ -0,0 +1,61 @@
+# Webscraper for COGS108 final project
+# Author: Timothy Walker
+# Date: 25 April 2019
+#
+# Notes: Links are in the following format;
+#
+#        https://www.billboard.com/archive/charts/2000/r-b-hip-hop-songs
+#
+#        For additional genres, add the last directory of the link. For
+#        example, the genre of the link above would be 'r-b-hip-hop-songs'.
+#
+
+# library imports
+import os, sys
+from urllib.request import Request, urlopen
+import bs4 as bs
+import pandas as pd
+
+# Set year and genres to scrape, ending year not inclusive
+dataPath = './data/'
+yearStart = 2000
+yearEnd = 2011
+genreList = ['r-b-hip-hop-songs', 'rap-song', 'country-songs',
+             'alternative-songs', 'hot-mainstream-rock-tracks', 
+             'adult-contemporary', 'adult-pop-songs', 'pop-songs',
+             'rhythmic-40', 'dance-club-play-songs', 'radio-songs',
+             'hot-100', 'billboard-200', 'latin-songs']
+
+
+for genre in genreList:
+
+    print('Grabbing genre: ' + genre)
+
+    if not os.path.exists('data'):
+            os.mkdir('data') 
+
+    # Write header
+    file = open('data/' + genre + ".csv", "w")
+    file.write('Year,Title,Artist\n')
+    file.close()
+
+    for year in range(yearStart, yearEnd):
+
+        print('year: ' + str(year))
+
+        # Grab html
+        req = Request('https://www.billboard.com/archive/charts/' + str(year) + '/' + genre + '/',
+                      headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        html = pd.read_html(webpage, header=0)  # dfs is a list of dataframes
+        df = html[0]  # df is a pandas datafram
+
+        # Rename columsn, drop dupplicate songs and add year
+        df.columns = ['Year', 'Title', 'Artist']
+        df.drop_duplicates(subset='Title', inplace=True)
+        df['Year'] = year
+     
+
+        # Print to .csv
+        df.to_csv(path_or_buf='./data/' + genre + '.csv',
+                  header=False, mode='a', index=False)
